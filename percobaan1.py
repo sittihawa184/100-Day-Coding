2089346d5a981cb952589f35ea8d4f09691e6703
sittihawa184 <hawahw184@gmail.com>
2022-10-17 13:43:32 +0800

    coba1

---
+++ percobaan1.py
@@ -0,0 +1,19 @@
+X = ("GAJI KARYAWAN")
+Y = ("NAMA = SITTI HAWA (D0220355)")
+Z = ("GAJI POKOK = Rp.100000")
+A =  ("GAJI LEMBUR = Rp. 550000")
+B = ("GAJIKOTOR =Rp.1055000")
+C = ("PAJAK = 0.09")
+D = ("POTONGAN = Rp. 90000")
+E = ("GAJI BERSIH = Rp. 965000")
+print(X)
+print(Y)
+print(Z)
+print(A)
+print(B)
+print(C)
+print(D)
+print(E)
+
+
+

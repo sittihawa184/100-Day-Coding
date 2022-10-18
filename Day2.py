b70e33cd0b16f257f5504efa46a1fea8c8d25797
sittihawa184 <hawahw184@gmail.com>
2022-10-18 16:40:23 +0800

    Day2

---
+++ Day2.py
@@ -0,0 +1,18 @@
+nilai =20
+nilai2 = 10
+nilai += 3 #(nilai + =2) sama  dengan (nilai = nilai+2)
+nilai2 =+ 3 #nilai2 =+ 2 sama dengan nilai2 =  3
+print(nilai)
+print(nilai2)
+nilai -= 2
+print (nilai)
+nilai *= 8
+print(nilai)
+nilai /= 2
+print(nilai)
+nilai %=5
+print(nilai)
+nilai **= 6
+print(nilai)
+nilai //= 4
+print(nilai)

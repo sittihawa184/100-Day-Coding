4cf251109fb01d4cae68ec99be3e3ced69d8f493
sittihawa184 <hawahw184@gmail.com>
2022-10-20 14:13:14 +0800

    Day_4

---
+++ Logika_Day_4.py
@@ -0,0 +1,12 @@
+Rajin = True
+Malas = True
+
+#logika AND
+Hawa = Rajin and Malas
+print (Malas, " AND ", Rajin, " = ", (Hawa))
+#logika OR
+Hawa = Malas or Rajin
+print (Rajin, " OR ", Malas, " = ",(Hawa))
+#logika NOT
+Hawa = not Malas
+print (" NOT ", Rajin, " = ", (Hawa))

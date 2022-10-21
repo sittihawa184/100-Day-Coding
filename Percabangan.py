7008b6c0ec0be063b1c9d008f4f24ea49e0ca1a2
sittihawa184 <hawahw184@gmail.com>
2022-10-21 19:35:56 +0800

    Day5

---
+++ Percabangan.py
@@ -0,0 +1,9 @@
+nilai = 85
+if (nilai >= 80) :
+    print("Sitti Hawa")
+nilai = 0
+if (nilai >= 0) :
+    print("Informatika")
+nilai = 1
+if (nilai >= 1) :
+    print("Kalukku")
---
+++ Percabangan2.py
@@ -0,0 +1,13 @@
+nilai = 85
+if (nilai >= 80) :
+    print("LULUS")
+else: 
+    print("Tidak Lulus")
+if (nilai <= 70) :
+    print("LULUS")
+else:
+    print("Tidak Lulus")
+if (nilai <= 60) :
+    print("Tidak Lulus")
+else:
+    print("Lulus")

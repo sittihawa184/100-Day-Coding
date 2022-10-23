129856a4529a653cf0d2d09111b5261b18368b0e
sittihawa184 <hawahw184@gmail.com>
2022-10-23 16:31:18 +0800

    Day7

---
+++ Percabangan_elif.py
@@ -0,0 +1,11 @@
+umur = int(input("Masukkan Umur = "))
+if(umur >18 and umur <30):
+    print("sudah beranjak dewasa")
+elif(umur>=30 and umur <45):
+    print("Masa Masa emas")
+elif(umur >= 45 and umur <55):
+    print("Masa Paruh Baya")
+elif(umur >= 50):
+    print("Masa Menua")
+else:
+    print("Masih Dibawah Umur")

470af5fff5a0add715488f85300c21eae731ff91
sittihawa184 <hawahw184@gmail.com>
2022-10-26 19:10:20 +0800

    Day_10

---
+++ Operator_Bitwise.py
@@ -0,0 +1,14 @@
+x = 100
+y = 112
+
+print('x berisi angka',x ,'desimal atau',bin(x),'biner')
+print('y berisi angka',y ,'desimal atau',bin(y),'biner')
+
+print('\n')
+
+print('x & y  :',x & y)
+print('x | y  :',x | y)
+print('x ^ y  :',x ^ y)
+print('~x     :',x)
+print('x << 1 :',x << 1)
+print('x >> 1 :',x >> 1)

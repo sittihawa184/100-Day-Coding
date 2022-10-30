35597e537af296c67ea924bfbc61e76e54fd944b
sittihawa184 <hawahw184@gmail.com>
2022-10-31 07:52:32 +0800

    Day 15

---
+++ fungsi argument.py
@@ -0,0 +1,18 @@
+def hello_world(ulasan):
+    '''fungsi hello world menerima input dengan variabel ulasan'''
+    print(f"dalam 100 hari ngoding ini serasa ada saja tantangan bagi kami, \n dan tentunya banyak pelajaran yang akan kami dapatkan \n Semoga sampai pada hari ke-100 ")
+hello_world("ulasan")
+def hello_world(nama):
+    '''fungsi hello world menerima input dengan variabel ulasan'''
+    print(f"Semangat setiap hari ngoding {nama} ")
+hello_world("Hawa")
+hello_world("Waskia")
+hello_world("Arfan")
+
+#program tambah
+def tambah(angka_1, angka_2):
+    '''fungsi tambah'''
+    hasil=angka_1 + angka_2
+    print(f"{angka_1} + {angka_2} = {hasil} ")
+tambah(7,29)
+tambah(2930,1248)

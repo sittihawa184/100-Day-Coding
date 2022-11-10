4b217b47b2fccfcba5acafef74a632c8085ae680
sittihawa184 <hawahw184@gmail.com>
2022-11-10 22:05:55 +0800

    Day 25

--- Button komponen pada tkinter.py
+++ Button komponen pada tkinter.py
@@ -2,11 +2,11 @@
 import tkinter
 from tkinter import *
 syp =  tkinter.Tk()
-syp.title('membuat button')
+syp.title('Tampilan')
 syp.geometry('200x400')
 
 def mahasiswa():
-    print("Tetap semangat :)")
+    print("Tetap semangat...!!!")
 
 btn = Button(syp, text="Merhaba", fg="#0000FF", padx=12, command=mahasiswa)
 btn.pack()
---
+++ Tabel Perkalian.py
@@ -0,0 +1,6 @@
+#Mengimport angka
+Z = int(input("Mulai dari perkalian : "))
+#Menghitung 10 Kali dari 1 sampai 10
+for i in range(1,11):
+#Menampilkan Tabel Perkalian
+    print(Z, 'x', i, '=', Z*i)

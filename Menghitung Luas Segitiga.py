c909dac2c8ebb2a17cfad1d7692e5a36cdb75d51
sittihawa184 <hawahw184@gmail.com>
2022-11-19 07:23:55 +0800

    Day 33

---
+++ Menghitung Luas Segitiga.py
@@ -0,0 +1,9 @@
+# Menginput Alas dan Tinggi 
+alas = float(input("Tulis Alas Segitiga: "))
+tinggi = float(input("Tulis Tinggi Segitiga: "))
+ 
+# Hitung Luas Segitiga
+luas = (alas * tinggi) / 2
+ 
+#Menampilkan Hasil 
+print("Luas Segitiga adalah %0.2f" %luas)
